04ddbf3c009b81f5b807ecd7156eb66931833fdc
Doodl0 <52604696+Doodl0@users.noreply.github.com>
2025-08-10 20:37:52 +0100

    Implemented basic ray tracing with a global ground plane and a sphere

--- scripts/ray_tracer.gd
+++ scripts/ray_tracer.gd
@@ -1,37 +1,48 @@
 extends Node
 
+@export var skybox_texture: Texture
 var viewport_dimensions: Vector2i
-@onready var output_display: RenderOutput = $"Camera3D/Output Display"
+@onready var output_display: RenderOutput = $%"Output Display"
 
 # Create local rendering device to run compute shaders on
 var rd := RenderingServer.create_local_rendering_device()
 var shader_rid: RID
 @export_file("*.glsl") var shader_file: String
 var render_rid: RID
+var skybox_rid: RID
 var uniform_set: RID
 var pipeline: RID
+var uniform_bindings: Array[RDUniform]
 
 func _ready() -> void:
+	# Set the render dimensions to the viewport dimensions so that the image is not stretched
 	viewport_dimensions.x = ProjectSettings.get_setting("display/window/size/viewport_width")
 	viewport_dimensions.y = ProjectSettings.get_setting("display/window/size/viewport_height")
 	
+	# Set the display image size to the same as the viewport
 	output_display.image_size = viewport_dimensions
+	# Create the texture so that data can be added later
 	output_display.init_texture()
 	
 	setup_compute()
 	render()
-	
+
+
 func _process(delta: float) -> void:
-	pass
+	update_compute()
+	render()
 
 func render():
+	# Validate that the pipeline has been created so to avoid errors
 	if pipeline == null:
 		setup_compute()
 	
+	# Create a compute list and bind uniforms and pipeline
 	var compute_list := rd.compute_list_begin()
 	rd.compute_list_bind_compute_pipeline(compute_list, pipeline)
 	rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0)
-	# Dispatch one workgroup for every 8x8 block of pixels here. This ratio is highly tunable, and performance may vary.
+	# Dispatch one workgroup for every 8x8 block of pixels here
+	# Ignore integer division to avoid warnings as both viewport and 8 are ints
 	@warning_ignore("integer_division")
 	rd.compute_list_dispatch(compute_list, viewport_dimensions.x / 8, viewport_dimensions.y / 8, 1)
 	rd.compute_list_end()
@@ -43,6 +54,7 @@ func render():
 	# Retrieve render data.
 	var render_bytes := rd.texture_get_data(render_rid, 0)
 	output_display.display_render(render_bytes)
+
 
 func setup_compute():
 	# Prepare the shader.
@@ -68,10 +80,76 @@ func setup_compute():
 	render_uniform.binding = 0
 	render_uniform.add_id(render_rid)
 	
+	# Create matrix from camera transform
+	var camera_to_world := get_viewport().get_camera_3d().global_transform
+	var basis := camera_to_world.basis
+	var origin := camera_to_world.origin
+	var camera_to_world_matrix := PackedFloat32Array([
+		basis.x.x, basis.x.y, basis.x.z, 1.0,
+		basis.y.x, basis.y.y, basis.y.z, 1.0,
+		basis.z.x, basis.z.y, basis.z.z, 1.0,
+		origin.x, origin.y, origin.z, 1.0
+	]).to_byte_array()
+	
+	# Create a projection matrix from camera projection
+	var projection := (get_viewport().get_camera_3d().get_camera_projection().inverse() * Projection.create_depth_correction(true))
+	print(projection)
+	var projection_matrix := PackedFloat32Array([
+		projection.x.x, projection.x.y, projection.x.z, projection.x.w,
+		projection.y.x, projection.y.y, projection.y.z, projection.y.w,
+		projection.z.x, projection.z.y, projection.z.z, projection.z.w,
+		projection.w.x, projection.w.y, projection.w.z, projection.w.w,
+	]).to_byte_array()
+	
+	var camera_data_bytes := PackedByteArray()
+	camera_data_bytes.append_array(camera_to_world_matrix)
+	camera_data_bytes.append_array(projection_matrix)
+	var camera_data_buffer := rd.storage_buffer_create(camera_data_bytes.size(), camera_data_bytes)
+	var camera_data_uniform := RDUniform.new()
+	camera_data_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	camera_data_uniform.binding = 1
+	camera_data_uniform.add_id(camera_data_buffer)
+	
+	uniform_bindings = [render_uniform, camera_data_uniform]
+	
 	# Create the set of uniforms from list of created uniforms and shader RID
-	uniform_set = rd.uniform_set_create([render_uniform], shader_rid, 0)
+	uniform_set = rd.uniform_set_create(uniform_bindings, shader_rid, 0)
 	# Create compute shader pipeline
 	pipeline = rd.compute_pipeline_create(shader_rid)
+
+func update_compute():
+	# Create matrix from camera transform
+	var camera_to_world := get_viewport().get_camera_3d().global_transform
+	var basis := camera_to_world.basis
+	var origin := camera_to_world.origin
+	var camera_to_world_matrix := PackedFloat32Array([
+		basis.x.x, basis.x.y, basis.x.z, 1.0,
+		basis.y.x, basis.y.y, basis.y.z, 1.0,
+		basis.z.x, basis.z.y, basis.z.z, 1.0,
+		origin.x, origin.y, origin.z, 1.0
+	]).to_byte_array()
+	
+	# Create a projection matrix from camera projection
+	var projection := (get_viewport().get_camera_3d().get_camera_projection().inverse() * Projection.create_depth_correction(true))
+	print(projection)
+	var projection_matrix := PackedFloat32Array([
+		projection.x.x, projection.x.y, projection.x.z, projection.x.w,
+		projection.y.x, projection.y.y, projection.y.z, projection.y.w,
+		projection.z.x, projection.z.y, projection.z.z, projection.z.w,
+		projection.w.x, projection.w.y, projection.w.z, projection.w.w,
+	]).to_byte_array()
+	
+	var camera_data_bytes := PackedByteArray()
+	camera_data_bytes.append_array(camera_to_world_matrix)
+	camera_data_bytes.append_array(projection_matrix)
+	var camera_data_buffer := rd.storage_buffer_create(camera_data_bytes.size(), camera_data_bytes)
+	var camera_data_uniform := RDUniform.new()
+	camera_data_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	camera_data_uniform.binding = 1
+	camera_data_uniform.add_id(camera_data_buffer)
+	
+	uniform_bindings[1] = camera_data_uniform
+	uniform_set = rd.uniform_set_create(uniform_bindings, shader_rid, 0)
 
 # Import, compile and load shader, return reference.
 func load_shader(p_rd: RenderingDevice, path: String) -> RID:
--- shaders/ray_tracer.glsl
+++ shaders/ray_tracer.glsl
@@ -3,11 +3,118 @@
 
 layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 
+// Uniform to render image to write and read
 layout(rgba32f, binding = 0) uniform image2D image_render;
+
+// Binding to camera data buffer
+layout(set = 0, binding = 1, std430) restrict buffer CameraData {
+    mat4 CameraToWorld;
+    mat4 CameraProjection;
+}
+camera_data;
+
+struct Ray {
+    vec3 origin;
+    vec3 direction;
+};
+
+struct RayHit
+{
+    vec3 position;
+    float distance;
+    vec3 normal;
+};
+
+const float PI = 3.14159265f;
+const float INF = 99999999.0;
+const vec3 sky_color = vec3(0.761, 0.965, 1);
+
+Ray CreateRay(vec3 origin, vec3 direction) {
+    Ray ray;
+    ray.origin = origin;
+    ray.direction = direction;
+    return ray;
+}
+
+Ray CreateCameraRay(vec2 uv) {
+    // Transform the camera origin to world space
+    vec3 origin = camera_data.CameraToWorld[3].xyz;
+    
+    // Invert the perspective projection of the view-space position
+    vec3 direction = (camera_data.CameraProjection * vec4(uv, 0.0, 1.0)).xyz;
+    // Transform the direction from camera to world space and normalize
+    direction = (camera_data.CameraToWorld * vec4(direction, 0.0)).xyz;
+    direction = normalize(direction);
+    return CreateRay(origin, direction);
+}
+
+RayHit CreateRayHit() {
+    RayHit hit;
+    hit.position = vec3(0.0f, 0.0f, 0.0f);
+    hit.distance = INF;
+    hit.normal = vec3(0.0f, 0.0f, 0.0f);
+    return hit;
+}
+
+void IntersectGroundPlane(Ray ray, inout RayHit bestHit) {
+    // Calculate distance along the ray where the ground plane is intersected
+    float t = -ray.origin.y / ray.direction.y;
+    if (t > 0 && t < bestHit.distance) {
+        bestHit.distance = t;
+        bestHit.position = ray.origin + t * ray.direction;
+        bestHit.normal = vec3(0.0f, 1.0f, 0.0f);
+    }
+}
+
+void IntersectSphere(Ray ray, inout RayHit bestHit, vec4 sphere)
+{
+    // Calculate distance along the ray where the sphere is intersected
+    vec3 d = ray.origin - sphere.xyz;
+    float p1 = -dot(ray.direction, d);
+    float p2sqr = p1 * p1 - dot(d, d) + sphere.w * sphere.w;
+    if (p2sqr < 0)
+        return;
+    float p2 = sqrt(p2sqr);
+    float t = p1 - p2 > 0 ? p1 - p2 : p1 + p2;
+    if (t > 0 && t < bestHit.distance)
+    {
+        bestHit.distance = t;
+        bestHit.position = ray.origin + t * ray.direction;
+        bestHit.normal = normalize(bestHit.position - sphere.xyz);
+    }
+}
+
+RayHit Trace(Ray ray)
+{
+    RayHit bestHit = CreateRayHit();
+    IntersectGroundPlane(ray, bestHit);
+    // Add a floating unit sphere
+    IntersectSphere(ray, bestHit, vec4(0, 3.0f, 0, 1.0f));
+    return bestHit;
+}
+
+vec3 Shade(inout Ray ray, RayHit hit) {
+    if (hit.distance < INF) {
+        // Return the normal
+        return hit.normal;
+    }
+    else {
+        return sky_color;
+    }
+}
 
 void main() {
     ivec2 pos = ivec2(gl_GlobalInvocationID.xy);
     ivec2 image_size = imageSize(image_render);
-    vec4 colour = vec4(float(pos.x) / float(image_size.x), float(pos.y) / float(image_size.y), 0, 1);
-    imageStore(image_render, pos, colour);
+    
+    // Transform pixels to [-1, 1] range
+    vec2 uv = vec2((pos.xy) / vec2(image_size) * 2.0f - 1.0f);
+
+    // Create a ray for the UVs
+    Ray ray = CreateCameraRay(uv);
+
+    RayHit hit = Trace(ray);
+    vec3 result = Shade(ray, hit);
+
+    imageStore(image_render, pos, vec4(result, 1.0));
 }
